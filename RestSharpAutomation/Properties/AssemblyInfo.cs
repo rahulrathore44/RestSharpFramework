37d74d21b65646770282f9c42469f0cc48bb4c17
Rathore <RahulSingh.Rathore@in.pega.com>
2019-09-29 21:06:53 -0700

    Added rensettings file

---
+++ RestSharpAutomation/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RestSharpAutomation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RestSharpAutomation")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4eb549c9-6742-45c8-a745-a93239cab6bd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- WebServiceAutomation/ParallelExecution/TestParallelExecution.cs
+++ WebServiceAutomation/ParallelExecution/TestParallelExecution.cs
@@ -21,20 +21,22 @@ namespace WebServiceAutomation.ParallelExecution
         private string delayPutUrl = "http://localhost:8080/laptop-bag/webapi/delay/update";
         private string xmlMediaType = "application/xml";
         private Random random = new Random();
+        private HttpClientAsyncHelper httpClientAsyncHelper = new HttpClientAsyncHelper();
 
         private void SendGetRequest()
         {
             Dictionary<string, string> httpHeader = new Dictionary<string, string>();
             httpHeader.Add("Accept", "application/json");
 
-            RestResponse restResponse = HttpClientHelper.PerformGetRequest(delayGetUrl, httpHeader);
+            RestResponse restResponse = httpClientAsyncHelper.PerformGetRequest(delayGetUrl, httpHeader).GetAwaiter().GetResult();
 
             //List<JsonRootObject> jsonRootObject = JsonConvert.DeserializeObject<List<JsonRootObject>>(restResponse.ResponseContent);
             //Console.WriteLine(jsonRootObject[0].ToString());
+            Assert.AreEqual(200, restResponse.StatusCode);
 
             List<JsonRootObject> jsonData = ResponseDataHelper.DeserializeJsonResponse<List<JsonRootObject>>(restResponse.ResponseContent);
 
-            Console.WriteLine(jsonData.ToString());
+            Console.WriteLine(jsonData[0].ToString());
         }
 
         private void SendPostRequest()
@@ -58,13 +60,14 @@ namespace WebServiceAutomation.ParallelExecution
                 { "Accept","application/xml"}
             };
 
-            RestResponse restResponse = HttpClientHelper.PerformPostRequest(delayPostUrl, xmlData, xmlMediaType, headers);
+            RestResponse restResponse = httpClientAsyncHelper.PerformPostRequest(delayPostUrl, xmlData, xmlMediaType, headers).GetAwaiter().GetResult();
 
             //HttpContent httpContent = new StringContent(xmlData, Encoding.UTF8, xmlMediaType) ;
             //HttpClientHelper.PerformPostRequest(postUrl, httpContent, headers);
 
             Assert.AreEqual(200, restResponse.StatusCode);
 
+            Console.WriteLine(">>>>>>>>>>" + restResponse.ResponseContent);
 
             Laptop xmlDatat = ResponseDataHelper.DeserializeXmlResponse<Laptop>(restResponse.ResponseContent);
             Console.WriteLine(xmlDatat.ToString());
@@ -91,7 +94,7 @@ namespace WebServiceAutomation.ParallelExecution
                 { "Accept","application/xml"}
             };
 
-            RestResponse restResponse = HttpClientHelper.PerformPostRequest(delayPostUrl, xmlData, xmlMediaType, headers);
+            RestResponse restResponse = httpClientAsyncHelper.PerformPostRequest(delayPostUrl, xmlData, xmlMediaType, headers).GetAwaiter().GetResult();
             Assert.AreEqual(200, restResponse.StatusCode);
 
             xmlData = "<Laptop>" +
@@ -107,10 +110,10 @@ namespace WebServiceAutomation.ParallelExecution
                                   "<LaptopName>Alienware M17</LaptopName>" +
                                "</Laptop>";
 
-            restResponse = HttpClientHelper.PerformPutRequest(delayPutUrl, xmlData, xmlMediaType, headers);
+            restResponse = httpClientAsyncHelper.PerformPutRequest(delayPutUrl, xmlData, xmlMediaType, headers).GetAwaiter().GetResult();
             Assert.AreEqual(200, restResponse.StatusCode);
 
-            restResponse = HttpClientHelper.PerformGetRequest(delayGetWithId + id, headers);
+            restResponse = httpClientAsyncHelper.PerformGetRequest(delayGetWithId + id, headers).GetAwaiter().GetResult();
             Assert.AreEqual(200, restResponse.StatusCode);
 
             Laptop xmlObj = ResponseDataHelper.DeserializeXmlResponse<Laptop>(restResponse.ResponseContent);
@@ -141,6 +144,100 @@ namespace WebServiceAutomation.ParallelExecution
             get.Wait();
             put.Wait();
             post.Wait();
+        }
+
+        [TestMethod]
+        public void TestTaskUsingTaskFactory()
+        {
+            var getTask = Task.Factory.StartNew(() =>
+            {
+                SendGetRequest();
+            });
+
+            var postTask = Task.Factory.StartNew(() =>
+            {
+                SendPostRequest();
+            });
+
+            var putTask = Task.Factory.StartNew(() =>
+            {
+                SendPutRequest();
+            });
+
+            getTask.Wait();
+            postTask.Wait();
+            putTask.Wait();
+
+        }
+
+        [TestMethod]
+        public void TestTaskUsingTaskFactoryWithResponse()
+        {
+            Task<RestResponse> getTask = Task.Factory.StartNew<RestResponse>(() =>
+            {
+                return SendGetRequestWithResponse();
+            });
+
+            Task<RestResponse> postTask = Task.Factory.StartNew<RestResponse>(() =>
+            {
+                return SendPostRequestWithResponse();
+            });
+
+            Console.WriteLine(getTask.Result.ResponseContent);
+            Console.WriteLine(postTask.Result.ResponseContent);
+
+        }
+
+        private RestResponse SendGetRequestWithResponse()
+        {
+            Dictionary<string, string> httpHeader = new Dictionary<string, string>();
+            httpHeader.Add("Accept", "application/json");
+
+            RestResponse restResponse = httpClientAsyncHelper.PerformGetRequest(delayGetUrl, httpHeader).GetAwaiter().GetResult();
+
+            //List<JsonRootObject> jsonRootObject = JsonConvert.DeserializeObject<List<JsonRootObject>>(restResponse.ResponseContent);
+            //Console.WriteLine(jsonRootObject[0].ToString());
+            Assert.AreEqual(200, restResponse.StatusCode);
+
+            //List<JsonRootObject> jsonData = ResponseDataHelper.DeserializeJsonResponse<List<JsonRootObject>>(restResponse.ResponseContent);
+
+            //Console.WriteLine(jsonData[0].ToString());
+            return restResponse;
+        }
+
+        private RestResponse SendPostRequestWithResponse()
+        {
+            int id = random.Next(1000);
+            string xmlData = "<Laptop>" +
+                                     "<BrandName>Alienware</BrandName>" +
+                                     "<Features>" +
+                                        "<Feature>8th Generation Intel® Core™ i5 - 8300H</Feature>" +
+                                        "<Feature>Windows 10 Home 64 - bit English</Feature>" +
+                                        "<Feature>NVIDIA® GeForce® GTX 1660 Ti 6GB GDDR6</Feature>" +
+                                        "<Feature>8GB, 2x4GB, DDR4, 2666MHz</Feature>" +
+                                      "</Features>" +
+                                   "<Id> " + id + "</Id>" +
+                                   "<LaptopName>Alienware M17</LaptopName>" +
+                                "</Laptop>";
+
+
+            Dictionary<string, string> headers = new Dictionary<string, string>()
+            {
+                { "Accept","application/xml"}
+            };
+
+            RestResponse restResponse = httpClientAsyncHelper.PerformPostRequest(delayPostUrl, xmlData, xmlMediaType, headers).GetAwaiter().GetResult();
+
+            //HttpContent httpContent = new StringContent(xmlData, Encoding.UTF8, xmlMediaType) ;
+            //HttpClientHelper.PerformPostRequest(postUrl, httpContent, headers);
+
+            Assert.AreEqual(200, restResponse.StatusCode);
+
+            Console.WriteLine(">>>>>>>>>>" + restResponse.ResponseContent);
+
+            //Laptop xmlDatat = ResponseDataHelper.DeserializeXmlResponse<Laptop>(restResponse.ResponseContent);
+            //Console.WriteLine(xmlDatat.ToString());
+            return restResponse;
         }
     }
 }
