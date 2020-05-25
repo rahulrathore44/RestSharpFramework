7c363840ee5cee8b1f10b612aa8466e99154e2b8
rathr1 <rathr1@L33986WUS>
2020-05-24 17:22:09 -0700

    Code for file upload

---
+++ RestSharpAutomation/RestPostEndpoint/FileUploadEndPoint.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestSharpAutomation.RestPostEndpoint
+{
+    [TestClass]
+    public class FileUploadEndPoint
+    {
+        [TestMethod]
+        public void Test_Upload_Of_File()
+        {
+            IRestClient client = new RestClient();
+            IRestRequest request = new RestRequest()
+            {
+                Resource = "https://jobportalkarate.herokuapp.com/normal/webapi/upload"
+            };
+
+            request.AddFile("file", @"C:\Users\rathr1\Desktop\PipeLine-Command.txt", "multipart/form-data");
+            var response = client.Post(request);
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+        }
+    }
+}
