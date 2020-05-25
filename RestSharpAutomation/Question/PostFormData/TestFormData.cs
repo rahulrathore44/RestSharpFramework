80dffbb9288d4ac92a14586f4120b69e5e23ce34
rathr1 <rathr1@L33986WUS>
2020-05-24 17:22:09 -0700

    Code for file upload

---
+++ RestSharpAutomation/Question/PostFormData/TestFormData.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestSharpAutomation.Question.PostFormData
+{
+    [TestClass]
+    public class TestFormData
+    {
+
+        [TestMethod]
+        public void Test_Post_of_Form_Data()
+        {
+            IRestClient client = new RestClient();
+            IRestRequest request = new RestRequest()
+            {
+                Resource = "https://api.eventbaja.com/api/v2/registration"
+
+            };
+           
+            request.AddParameter("role", "vendor", ParameterType.GetOrPost);
+            request.AddParameter("user", "test@mailinator", ParameterType.GetOrPost);
+            request.AddParameter("address", "89", ParameterType.GetOrPost);
+            request.AddParameter("password", "qwertyui", ParameterType.GetOrPost);
+            request.AddParameter("password_confirmation", "qwertyui", ParameterType.GetOrPost);
+            request.AddParameter("otp", "526524", ParameterType.GetOrPost);
+            request.AddParameter("confirm otp", "526524", ParameterType.GetOrPost);
+            
+
+            var response =  client.Post(request);
+            Console.WriteLine(response.StatusCode);
+        }
+    }
+}
---
+++ WebServiceAutomation/Question/TestNestedJson.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServiceAutomation.Question
+{
+    [TestClass]
+    public class TestNestedJson
+    {
+        [TestMethod]
+        public void TestUsingStatement()
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                using (HttpRequestMessage httpRequestMessage = new HttpRequestMessage())
+                {
+                    httpRequestMessage.RequestUri = new Uri("http://dummy.restapiexample.com/api/v1/employees");
+                    httpRequestMessage.Method = HttpMethod.Get;
+                    httpRequestMessage.Headers.Add("Accept", "application/json");
+                    Task<HttpResponseMessage> httpResponse = httpClient.SendAsync(httpRequestMessage);
+                    using (HttpResponseMessage httpResponseMessage = httpResponse.Result)
+                    {
+                        HttpStatusCode status = httpResponseMessage.StatusCode;
+                        using (HttpContent content = httpResponseMessage.Content)
+                        {
+                            using (Task<string> getResponse = content.ReadAsStringAsync())
+                            {
+                                string output = getResponse.Result;
+                                Employee datum = JsonConvert.DeserializeObject<Employee>(output);
+                                Console.WriteLine(datum.data[0].id);
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+
+    class Datum
+    {
+        public string id { get; set; }
+        public string employee_name { get; set; }
+        public string employee_salary { get; set; }
+        public string employee_age { get; set; }
+        public string profile_image { get; set; }
+    }
+
+    class Employee
+    {
+        public string status { get; set; }
+        public IList<Datum> data { get; set; }
+    }
+}
