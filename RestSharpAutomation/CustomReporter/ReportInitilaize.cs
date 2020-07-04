2f8d20006e07c7661c4a0805eebca86821f00a9b
rathr1 <rathr1@L33986WUS>
2020-07-04 16:40:12 -0700

    Custom Attribute

---
+++ RestSharpAutomation/CustomReporter/ReportInitilaize.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestSharpAutomation.CustomReporter
+{
+    [TestClass]
+    public class ReportInitilaize
+    {
+        [AssemblyCleanup]
+        public static void CleanUp()
+        {
+            CustomeExtentRepoter.GetInstance().WriteToReport();
+        }
+    }
+}
---
+++ RestSharpAutomation/ReportAttribute/TestCustomAttribue.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestSharpAutomation.ReportAttribute
+{
+    [TestClass]
+    public class TestCustomAttribue
+    {
+        [TestMethodWithReport]
+        public void FirstMethod()
+        {
+
+        }
+
+        [TestMethodWithReport]
+        public void SecondMethod()
+        {
+            Assert.Fail("I failed the test method");
+        }
+
+    }
+}
