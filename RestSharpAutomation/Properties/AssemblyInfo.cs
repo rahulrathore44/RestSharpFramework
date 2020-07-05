bdeadd5c95866c3d73593f7bb10f3820bff51069
rathr1 <rathr1@L33986WUS>
2020-07-05 12:39:29 -0700

    Code for Extent Report

--- RestSharpAutomation/Properties/AssemblyInfo.cs
+++ RestSharpAutomation/Properties/AssemblyInfo.cs
@@ -1,3 +1,4 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -18,3 +19,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+//[assembly: Parallelize(Workers = 4, Scope = Microsoft.VisualStudio.TestTools.UnitTesting.ExecutionScope.MethodLevel)]
