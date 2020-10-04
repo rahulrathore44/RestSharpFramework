9649423ed83f2138010fefc3cf98fb4d5fabb432
rathr1 <rathr1@L33986WUS>
2020-10-04 15:36:05 -0700

    Grapg QL

---
+++ RestSharpAutomation/GraphQL/TestGraphQL.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestSharpAutomation.GraphQL
+{
+    [TestClass]
+    public class TestGraphQL
+    {
+        private IRestClient client;
+        private IRestRequest request;
+        private string PostUrl = "http://localhost:4000/";
+
+        [TestMethod]
+        public void TestGraphQLQuery()
+        {
+            var QueryOne = @"query {
+                                allBooks {
+                                    id,
+                                    author,
+                                    title,
+                                    isbn,
+                                    url
+                                }
+                            }";
+
+            var QueryTwo = @"query {
+                                allBooks {
+                                    id,
+                                    author
+                                }
+                            }";
+
+            var QueryThree = @"query{
+                                allBooks(filter:{
+                                    id:17
+                                }) {
+                                    isbn
+                                }
+                            }";
+
+            var QueryFour = @"query findById($_id: ID){
+                                allBooks(filter:{
+                                    id:$_id
+                                }) {
+                                    isbn
+                                }
+                            }";
+
+            client = new RestClient();
+            request = new RestRequest()
+            {
+                Method = Method.POST,
+                Resource = PostUrl
+            };
+
+            request.AddHeader("Content-Type", "application/json");
+            request.AddJsonBody(new
+            {
+                query = QueryFour,
+                variables = new { _id = 17}
+            });
+            var response = client.Post(request);
+            Assert.AreEqual(200, (int)response.StatusCode);
+            Console.WriteLine(response.Content);
+        }
+    }
+}
