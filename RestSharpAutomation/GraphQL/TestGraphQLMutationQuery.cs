03965c8e1777d2a7a8567561b3d897a97820e722
rathr1 <rathr1@L33986WUS>
2020-11-23 11:31:51 -0800

    Added the retry

---
+++ RestSharpAutomation/GraphQL/TestGraphQLMutationQuery.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestSharpAutomation.GraphQL
+{
+    [TestClass]
+    public class TestGraphQLMutationQuery
+    {
+        private IRestClient client;
+        private IRestRequest request;
+        private string PostUrl = "http://localhost:4000/";
+
+        [TestMethod]
+        public void TestMutationQuery()
+        {
+            var QueryOne = @"query {
+                                allBooks {
+                                    id,
+                                    author,
+                                    title,
+                                    isbn,
+                                    url
+                                }
+                            }";
+
+            client = new RestClient();
+            request = new RestRequest()
+            {
+                Method = Method.POST,
+                Resource = PostUrl
+            };
+
+            request.AddHeader("Content-Type", "application/json");
+            request.AddJsonBody(new
+            {
+                query = QueryOne,
+                variables = new { _id = 17 }
+            });
+            var response = client.Post(request);
+            Assert.AreEqual(200, (int)response.StatusCode);
+            Console.WriteLine(response.Content);
+        }
+    }
+}
---
+++ RestSharpAutomation/Question/Attribute/TestMethodWithRetry.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestSharpAutomation.Question.Attribute
+{
+    [AttributeUsage(AttributeTargets.Method,AllowMultiple = false)]
+    public class TestMethodWithRetry : TestMethodAttribute
+    {
+        public override TestResult[] Execute(ITestMethod testMethod)
+        {
+            var result =  base.Execute(testMethod);
+            /**
+             * Check if there is a failed test then rerun it
+             */
+            if(result.Any(test => test.Outcome == UnitTestOutcome.Failed))
+            {
+                result = base.Execute(testMethod);
+            }
+
+            return result;
+        }
+    }
+}
