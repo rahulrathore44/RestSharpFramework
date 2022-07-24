b61a5d34884a9ba2f69a8293ecc26717e446e850
rahulrathore44 <rahulrathore44@LRRS>
2022-07-24 12:41:41 -0700

    Working with JSON and XML data

--- RestSharpLatest/GetRequest/TestGetRequest.cs
+++ RestSharpLatest/GetRequest/TestGetRequest.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RestSharp;
 using System.Diagnostics;
-
+using RestSharp.Serializers.Xml;
+using System.Collections.Generic;
+using WebServiceAutomation.Model.JsonModel;
 
 namespace RestSharpLatest.GetRequest
 {
@@ -20,7 +22,6 @@ namespace RestSharpLatest.GetRequest
             // Get, later - ExecuteGet
             RestResponse response = client.ExecuteGet(getRequest);
             // Console.WriteLine()
-
             // Debug Class
 
             // Status Code
@@ -32,5 +33,90 @@ namespace RestSharpLatest.GetRequest
             Debug.WriteLine($"Exception - {response.ErrorException}");
 
         }
+
+        [TestMethod]
+        public void GetRequestPrintResponseContent()
+        {
+            RestClient client = new RestClient();
+            RestRequest getRequest = new RestRequest(getUrl);
+            RestResponse response = client.ExecuteGet(getRequest);
+
+            if (response.IsSuccessful)
+            {
+                // Status Code
+                Debug.WriteLine($"Response Status Code - {response.StatusCode}");
+                // Content
+                Debug.WriteLine($"Response Content - {response.Content}");
+            }
+            else
+            {
+                // Error Message
+                Debug.WriteLine($"Error Message - {response.ErrorMessage}");
+                // Exception
+                Debug.WriteLine($"Exception - {response.ErrorException}");
+            }
+        }
+
+        [TestMethod]
+        public void GetRequestPrintResponseContentInXml()
+        {
+            RestClient client = new RestClient();
+            RestRequest getRequest = new RestRequest(getUrl);
+
+            // Add header
+            //getRequest.AddHeader("Accept", "application/xml");
+
+            client.UseXml();
+
+            RestResponse response = client.ExecuteGet(getRequest);
+
+            if (response.IsSuccessful)
+            {
+                // Status Code
+                Debug.WriteLine($"Response Status Code - {response.StatusCode}");
+                // Content
+                Debug.WriteLine($"Response Content - {response.Content}");
+            }
+            else
+            {
+                // Error Message
+                Debug.WriteLine($"Error Message - {response.ErrorMessage}");
+                // Exception
+                Debug.WriteLine($"Exception - {response.ErrorException}");
+            }
+        }
+
+
+        [TestMethod]
+        public void GetRequestPrintResponseContentInJson()
+        {
+            RestClient client = new RestClient();
+            RestRequest getRequest = new RestRequest(getUrl);
+
+            // Add header
+            //getRequest.AddHeader("Accept", "application/json");
+
+            client.UseJson(); // Default configuration
+
+            RestResponse response = client.ExecuteGet(getRequest);
+
+            if (response.IsSuccessful)
+            {
+                // Status Code
+                Debug.WriteLine($"Response Status Code - {response.StatusCode}");
+                // Content
+                Debug.WriteLine($"Response Content - {response.Content}");
+            }
+            else
+            {
+                // Error Message
+                Debug.WriteLine($"Error Message - {response.ErrorMessage}");
+                // Exception
+                Debug.WriteLine($"Exception - {response.ErrorException}");
+            }
+        }
+
+      
+
     }
 }
