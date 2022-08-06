6924cd2bbc74449b801133e6ff8dd9bcb825f3dc
rahulrathore44 <rahulrathore44@LRRS>
2022-08-06 16:43:02 -0700

    Added the Executor

---
+++ RestSharpLatest/APIHelper/RestApiExecutor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestSharpLatest.APIHelper
+{
+    public class RestApiExecutor
+    {
+        private ICommand Command;
+
+        public void SetCommand(ICommand _command)
+        {
+            Command = _command;
+        }
+
+        public IResponse ExecuteRequest()
+        {
+            return Command.ExecuteRequest();
+        }
+
+        public IResponse<T> ExecuteRequest<T>()
+        {
+            return Command.ExecuteRequest<T>();
+        }
+    }
+}
