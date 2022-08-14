006481b9b3d6d39d31e830f4e1d0f1173490b6e7
rahulrathore44 <rahulrathore44@LRRS>
2022-08-14 11:06:57 -0700

    Added Post Request Builder class

---
+++ RestSharpLatest/APIHelper/APIRequest/PostRequestBuilder.cs
@@ -0,0 +1,72 @@
+﻿using RestSharp;
+using RestSharp.Serializers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestSharpLatest.APIHelper.APIRequest
+{
+    public class PostRequestBuilder : AbstractRequest
+    {
+        private readonly RestRequest _restRequest;
+
+        public PostRequestBuilder()
+        {
+            _restRequest = new RestRequest()
+            {
+                Method = Method.Post
+            };
+        }
+
+        public override RestRequest Build()
+        {
+            return _restRequest;
+        }
+
+        //URL
+
+        public PostRequestBuilder WithUrl(string url)
+        {
+            WithUrl(url, _restRequest);
+            return this;
+        }
+
+        // Headers
+        public PostRequestBuilder WithHeaders(Dictionary<string, string> headers)
+        {
+            WithHeaders(headers, _restRequest);
+            return this;
+        }
+
+        // Body
+
+        public PostRequestBuilder WithBody<T>(T body, RequestBodyType bodyType, string contentType = ContentType.Json) where T : class
+        {
+            // String
+            // Object 
+
+            switch (bodyType)
+            {
+                case RequestBodyType.STRING:
+                    _restRequest.AddStringBody(body.ToString(), contentType);
+                    break;
+                case RequestBodyType.JSON:
+                    _restRequest.AddJsonBody<T>(body);
+                    break;
+                case RequestBodyType.XML:
+                    break;
+            }
+            return this;
+        }
+
+    }
+
+    public enum RequestBodyType
+    {
+        STRING, // For the String body
+        JSON, // Serialize the object in to JSON
+        XML // Serialize the object in to XML
+    }
+}
--- RestSharpLatest/PostRequest/TestPostRequest.cs
+++ RestSharpLatest/PostRequest/TestPostRequest.cs
@@ -5,6 +5,10 @@ using FluentAssertions;
 using System.Diagnostics;
 using WebServiceAutomation.Model.JsonModel;
 using RestSharpLatest.APIModel.JsonApiModel;
+using RestSharpLatest.APIHelper;
+using RestSharpLatest.APIHelper.Client;
+using RestSharpLatest.APIHelper.APIRequest;
+using RestSharpLatest.APIHelper.Command;
 
 namespace RestSharpLatest.PostRequest
 {
@@ -13,6 +17,21 @@ namespace RestSharpLatest.PostRequest
     {
         private string postUrl = "http://localhost:8081/laptop-bag/webapi/api/add";
         private Random random = new Random();
+        private static IClient _client;
+        private static RestApiExecutor _executor;
+
+        [ClassInitialize]
+        public static void Setup(TestContext testContext)
+        {
+            _client = new DefaultClient();
+            _executor = new RestApiExecutor();
+        }
+
+        [ClassCleanup]
+        public static void TearDown()
+        {
+            _client?.Dispose();
+        }
 
         [TestMethod]
         public void TestPostRequestWithStringBody()
@@ -47,7 +66,7 @@ namespace RestSharpLatest.PostRequest
             RestResponse response = client.ExecutePost(request);
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
             Debug.WriteLine(response.Content);
-            
+
         }
 
         [TestMethod]
@@ -78,6 +97,69 @@ namespace RestSharpLatest.PostRequest
             var response = client.ExecutePost(request);
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
             Debug.WriteLine(response.Content);
+        }
+
+        [TestMethod]
+        public void TestPostRequestWithFramework_Json()
+        {
+            int id = random.Next(1000);
+            var payload = new JsonModelBuilder().WithId(id).WithBrandName("Test BrandName").WithLaptopName("Test LaptopName").WithFeatures(new System.Collections.Generic.List<string>() { "Feature1", "Feature2" }).Build();
+
+            // Post Request
+
+            var request = new PostRequestBuilder().WithUrl(postUrl).WithBody<JsonModel>(payload, RequestBodyType.JSON);
+
+            // Command
+
+            var command = new RequestCommand(request, _client);
+            // SetCommand
+
+            _executor.SetCommand(command);
+            // Execute the request
+            var response = _executor.ExecuteRequest();
+            response.GetHttpStatusCode().Should().Be(System.Net.HttpStatusCode.OK);
+            response.GetResponseData().Should().Contain("Test LaptopName");
+        }
+
+
+        [TestMethod]
+        public void TestPostRequestWithFramework_String()
+        {
+            int id = random.Next(1000);
+            string jsonData = "{" +
+                                    "\"BrandName\": \"Alienware\"," +
+                                    "\"Features\": {" +
+                                    "\"Feature\": [" +
+                                    "\"8th Generation Intel® Core™ i5-8300H\"," +
+                                    "\"Windows 10 Home 64-bit English\"," +
+                                    "\"NVIDIA® GeForce® GTX 1660 Ti 6GB GDDR6\"," +
+                                    "\"8GB, 2x4GB, DDR4, 2666MHz\"" +
+                                    "]" +
+                                    "}," +
+                                    "\"Id\": " + id + "," +
+                                    "\"LaptopName\": \"Alienware M17\"" +
+                                "}";
+
+            // Post Request
+
+            var request = new PostRequestBuilder().WithUrl(postUrl).WithBody<string>(jsonData, RequestBodyType.STRING);
+
+            // Command
+
+            var command = new RequestCommand(request, _client);
+            // SetCommand
+
+            _executor.SetCommand(command);
+            // Execute the request
+            var response = _executor.ExecuteRequest();
+            response.GetHttpStatusCode().Should().Be(System.Net.HttpStatusCode.OK);
+            response.GetResponseData().Should().Contain("8th Generation Intel® Core™ i5-8300H");
+
+            var responseType = _executor.ExecuteRequest<JsonModel>();
+            responseType.GetHttpStatusCode().Should().Be(System.Net.HttpStatusCode.OK);
+            var responseData = responseType.GetResponseData();
+            responseData.Features.Feature.Should().Contain("8th Generation Intel® Core™ i5-8300H");
+
         }
     }
 }
