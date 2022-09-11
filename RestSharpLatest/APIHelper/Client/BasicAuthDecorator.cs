ec4fb04d9204b196309b6fc3a793ed74473754c6
rahulrathore44 <rahulrathore44@LRRS>
2022-09-11 13:08:03 -0700

    Client Decorator

---
+++ RestSharpLatest/APIHelper/Client/BasicAuthDecorator.cs
@@ -0,0 +1,30 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+
+namespace RestSharpLatest.APIHelper.Client
+{
+    public class BasicAuthDecorator : IClient
+    {
+        private readonly IClient _client;
+
+        public BasicAuthDecorator(IClient client) // Default Client // Tracer Client
+        {
+            _client = client; 
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
+        }
+
+        public RestClient GetClient()
+        {
+            //1. Invoke the GetClient API on the given client
+            var newClient = _client.GetClient(); // Plain Client , // Rest Client with tracer
+            //2. The set the authentication configuration
+            newClient.Authenticator = new HttpBasicAuthenticator("admin", "welcome");
+            //3. return the new client
+            return newClient; // plain Client + Basic Auth info, // Rest Client with tracer + Basic Auth info
+        }
+    }
+}
