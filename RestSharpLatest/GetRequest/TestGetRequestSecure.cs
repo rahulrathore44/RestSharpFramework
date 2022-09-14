201e70f9bb2246e755119dad134dae6f06f03df0
rahulrathore44 <rahulrathore44@LRRS>
2022-09-13 18:35:54 -0700

    Update TestGetRequestSecure.cs with Basic Auth Decorator

--- RestSharpLatest/GetRequest/TestGetRequestSecure.cs
+++ RestSharpLatest/GetRequest/TestGetRequestSecure.cs
@@ -2,6 +2,10 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RestSharp;
 using RestSharp.Authenticators;
+using RestSharpLatest.APIHelper;
+using RestSharpLatest.APIHelper.APIRequest;
+using RestSharpLatest.APIHelper.Client;
+using RestSharpLatest.APIHelper.Command;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +18,26 @@ namespace RestSharpLatest.GetRequest
     public class TestGetRequestSecure
     {
         private readonly string SecureGetUrl = "http://localhost:8081/laptop-bag/webapi/secure/all";
+
+        private static IClient _client;
+        private static IClient authClient;
+        private static RestApiExecutor apiExecutor;
+
+        [ClassInitialize]
+        public static void Setup(TestContext testContext)
+        {
+            // Created the Tracer client
+            _client = new TracerClient(); 
+            // Invok the decorator with tracer client impl
+            authClient = new BasicAuthDecorator(_client);
+            apiExecutor = new RestApiExecutor();
+        }
+
+        [ClassCleanup]
+        public static void TearDown()
+        {
+            _client?.Dispose();
+        }
 
         [TestMethod]
         public void TestGetRequestWithBasicAuth()
@@ -35,6 +59,23 @@ namespace RestSharpLatest.GetRequest
 
             var response = restClient.ExecuteGet(getRequest);
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+
+        }
+
+        [TestMethod]
+        public void TestSecureGetUsingDecorator()
+        {
+            //1. Create the request using Get Request builder class
+            var getRequest = new GetRequestBuilder().WithUrl(SecureGetUrl);
+            //2. Create the request command
+            var command = new RequestCommand(getRequest, authClient);
+            //3. Set the command on the API executor
+            apiExecutor.SetCommand(command);
+            //4. Execute the request
+            var response = apiExecutor.ExecuteRequest();
+            //5. Capture the response
+            //6. Add the validation on the response status code.
+            response.GetHttpStatusCode().Should().Be(System.Net.HttpStatusCode.OK);
 
         }
     }
