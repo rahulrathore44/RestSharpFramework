fd47418d96d10c7f09f09366a516cef9a4b1b617
rahulrathore44 <rahulrathore44@LRRS>
2022-10-09 16:39:19 +0530

    Parallel Download of the files

--- RestSharpLatest/DropBox/TestDownloadFile.cs
+++ RestSharpLatest/DropBox/TestDownloadFile.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RestSharp;
 using RestSharp.Authenticators;
+using System;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace RestSharpLatest.DropBox
 {
@@ -34,6 +37,50 @@ namespace RestSharpLatest.DropBox
             var data = client.DownloadData(request);
             File.WriteAllBytes(fileName, data);
             client.Dispose();
+        }
+
+        [TestMethod]
+        public void DownloadData_Parallel()
+        {
+            var contextPath = "/files/download";
+
+            var fileNameOne = "Video.mp4";
+            var locationOne = "{\"path\":\"/" + fileNameOne + "\"}";
+
+            var fileNameTwo = "Video.mp4";
+            var locationTwo = "{\"path\":\"/" + fileNameTwo + "\"}";
+
+            var client = new RestClient()
+            {
+                Authenticator = new JwtAuthenticator(Token)
+            };
+
+            var audioDownload = new RestRequest()
+            {
+                Resource = BasePath + contextPath,
+                Method = Method.Post
+            };
+
+            audioDownload.AddHeader("Dropbox-API-Arg", locationTwo);
+
+            var videoDownload = new RestRequest()
+            {
+                Resource = BasePath + contextPath,
+                Method = Method.Post
+            };
+            videoDownload.AddHeader("Dropbox-API-Arg", locationOne);
+
+            var task1 = client.DownloadDataAsync(videoDownload);
+            var task2 = client.DownloadDataAsync(audioDownload);
+
+            Task.WaitAll(task1, task2);
+
+            File.WriteAllBytes(fileNameOne, task1.Result);
+            File.WriteAllBytes(fileNameTwo, task2.Result);
+
+            File.Exists(fileNameOne).Should().BeTrue();
+            File.Exists(fileNameTwo).Should().BeTrue();
+
         }
     }
 }
